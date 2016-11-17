1fd1f4202bcf20e562c5f49060dba03c66a1cb62
Ales Katona <almindor@gmail.com>
2016-11-16 23:43:25 -0700

    * bump to 1.4.6

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ var app = express();
 var jsonParser = bodyParser.json();
 var lastRequest = new Date();
 var currencies;
-var version = '1.4.5';
+var version = '1.4.6';
 
 app.use( jsonParser );
 
