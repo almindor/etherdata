d1e735117bb2c74c890f4438f20a6e9c767868d1
Aleš Katona <almindor@gmail.com>
2017-06-08 09:31:51 +0200

    * bump min version to 1.5.0

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ var app = express();
 var jsonParser = bodyParser.json();
 var lastRequest = new Date();
 var currencies;
-var version = '1.4.9';
+var version = '1.5.0';
 
 app.use( jsonParser );
 
